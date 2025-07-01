98ade55b37118aebde3575ec51cd580c90bfde0b
Muharram <ozodboyevamuharram129@gmail.com>
2025-07-01 22:57:49 +0500

    requests

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
+//Get POst 
 const express = require('express')
 const app = express()
+app.get('/', (req, res) => res.json({ message: 'Hello' }))
+
 const PORT = 8080
 
-app.listen(PORT, ()=>console.log(`Listening on- https//localhost ${PORT}`))
+console.log('Hello world')
+app.get('/post', (req, res) => res.status(200).json({ message: 'Hey' }))
+
+app.listen(PORT, () => console.log(`Listening on - http://localhost:${PORT}`))
