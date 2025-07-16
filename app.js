9661c941aa2ad0a5ffc4331708d1fe87bf2c5894
Muharram <ozodboyevamuharram129@gmail.com>
2025-07-16 12:05:05 +0500

    config of .env

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 // Http methods haqida o'rganish
+require("dotenv").config()
 const express = require('express')
 const mongoose = require('mongoose')
 const postModel = require('./models/post.model')
 const app = express()
 
-const PORT = 8080
+const PORT = process.env.PORT || 8080
 
 // so'rov olish uchun ishlatiladi 
 app.get('/', async (req, res) => {
@@ -35,12 +36,12 @@ app.delete('/:id', (req, res) => {
 })
 
 //MongoDb LInkimiz
-const DB_URL = `mongodb+srv://sammi:AOqTaiUPEtEkckTZ@cluster0.jqbujny.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+const DB_URL = process.env.DB_URL
 
 //Ma'lumotlar bazasiga ulanish
 const bootstrap = async () => {
 	try {
-		await mongoose.connect(DB_URL).then(() => console.log(`Connected to Db`))
+		await mongoose.connect(process.env.DB_URL).then(() => console.log(`Connected to Db`))
 		app.listen(PORT, () =>
 			console.log(`Listening on - http://localhost:${PORT}`)
 		)
